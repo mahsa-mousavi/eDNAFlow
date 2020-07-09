89c8b91ef6b780ea6ccdb2aa8a79fc759225d317
Mahsa Mousavi <mahsamousavi88@yahoo.com>
2020-07-09 12:57:52 +0800

    1st commit default parameters of lulu

---
+++ lulu.R
@@ -0,0 +1,23 @@
+# This is a lulu script for post-clustering of Zotu table created by unoise3
+
+require(lulu)
+otutab <- read.csv("zotuTable.txt",sep='\t',header=TRUE,as.is=TRUE, row.names = 1)
+matchlist <- read.table("match_list.txt", header=FALSE,as.is=TRUE, stringsAsFactors=FALSE)
+
+# Curation step with lulu
+curated_result <- lulu(otutab, matchlist) # This runs the default parameter of lulu (i.e. minimum_ratio_type = "min", minimum_ratio = 1, minimum_match = 84, minimum_relative_cooccurence = 0.95)
+# there are more parameters to play with the in lulu command 
+
+curated_result$curated_table # Curated OTU table
+curated_result$curated_count # Number of OTUs retained
+curated_result$curated_otus # IDs of curated OTUs
+curated_result$discarded_count # OTUs discarded
+curated_result$otu_map # total - total read count, spread - the number of samples the OTU is present in
+                       # parent_id - ID of OTU with which this OTU was merged (or self)
+                       # curated - ("parent" or "merged"), was this OTU kept as a valid OTU (parent) or merged with another
+                       # rank - The rank of the OTU in terms of decreasing spread and read count
+
+curated_result$original_table # Original OTU table
+
+write.table(curated_result$curated_table, "curated_zotuTable.tab", sep="\t")  # write curated result 
+write.table(curated_result$otu_map, "lulu_zotu_map.tab", sep="\t")            # write the map info
